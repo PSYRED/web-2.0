e56874f5f2cb566a5ce410c8f1ca0c88684cbf85
unknown <upkabalisa@gmail.com>
2026-04-06 21:12:54 +0200

    fix:magicLink-implementation

--- src/pages/Sign_up.jsx
+++ src/pages/Sign_up.jsx
@@ -72,7 +72,7 @@ export default function Login() {
                 
       // Sign-up with e-mail verification
        
-        const {data,error} = await supabase.auth.signUp({
+        const {data,error} = await supabase.auth.signInWithOtp({
           email,
           options :{
             emailRedirectTo:'https://psyred.com/auth/Callback'
