d4fd1bfdded46360c11628d0ec7b5e983bb02140
unknown <upkabalisa@gmail.com>
2026-04-08 19:04:46 +0200

    fix:subfolders

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 import {imagetools} from 'vite-imagetools'
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  
   plugins: [react(),imagetools()],
 })
